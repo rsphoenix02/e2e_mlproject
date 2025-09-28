6e97b7967b247df3a537eb4ac0ab7264fa3c70ab
rsphoenix02 <saq.saf0501@gmail.com>
2025-09-28 23:42:52 +0530

    Updated requirements

--- application.py
+++ application.py
@@ -6,14 +6,13 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
-app = application
 ## Route for a home page
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@application.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -40,4 +39,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    application.run(host="0.0.0.0", port=5000, debug=False)        
